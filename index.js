0b2ba49af3a740a3da76a6942ee403b2286bae00
Graham Wood <graham@MacBook-Pro-2.local>
2017-04-26 15:23:52 -0500

    Added function to generate a Base 64 hash to be used for the shortened link text.

--- index.js
+++ index.js
@@ -3,29 +3,50 @@
 require('dotenv').config();
 const express = require('express');
 const chalk = require('chalk');
+const Base62 = require('base62');
+const fnv = require('fnv-plus');
+const moment = require('moment');
+
 
 // listen on port set in env in deveopment or just port 80 if no env files exist
 const port = process.env.PORT || 80;
 
 const app = express();
 
-// Listen for
 app.get('/new/*', (req, res) => {
 
-  res.send(req.params[0]);
+  // test code
+  res.send({ 'hash' : encode(req.params[0]) });
 
 });
 
 app.get('/:id', (req, res) => {
 
   res.send(req.params);
+
 });
 
 app.get('/', (req, res) => {
 
   res.end('TODO: Home Page');
+
 });
 
 app.listen(port, () => {
   console.log(chalk.yellow(`Listening on port: ${port}`));
 });
+
+
+// generate a hash by first taking a string + timestamp and hashing it with the
+// FNV algorithm, then chaging that integer to base62.
+// May not be unique.
+function encode(string) {
+
+    // add unix timestamp to string
+    const seededString = moment().format('x') + string;
+
+    // FNV then Base62
+    const hash = Base62.encode(fnv.hash(seededString).dec());
+
+    return hash;
+}
